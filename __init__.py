087e3f7dd60dfd2f5ebf79ae5b7b88e3b3587aef
Hexiang Qi <qihexiang@outlook.com>
2023-11-22 16:28:02 +0800

    aiohttp

--- __init__.py
+++ __init__.py
@@ -1,44 +1,63 @@
 from typing import TypedDict
-from requests import Session
+import aiohttp
 
-session = Session()
+class Atom(TypedDict):
+    element: int
+    position: list[float, float, float]
 
-class Config(TypedDict):
-    prefix: str
+type Atoms = dict[int, Atom | None]
 
-config: Config = {
-    "prefix": "http://localhost:10810"
-}
+class Bonds(TypedDict):
+    indexes: list[tuple[int, int]]
+    values: list[float | None]
+
+class Molecule(TypedDict):
+    atoms: Atoms
+    bonds: Bonds
 
 class Workspace:
-    def __init__(self, name: str) -> None:
+    def __init__(self, server: str, name: str) -> None:
         self.__name__ = name
+        self.__session__ = aiohttp.ClientSession(f"{server}")
 
-    def __request__(self, method: str, path: str, *args, **kargs):
-        return session.request(method, f"{config['prefix']}/workspaces/{self.__name__}{path}", *args, **kargs)
+    def __request__(self, method: str, path: str, **kargs):
+        return self.__session__.request(method, f"/workspaces/{self.__name__}{path}", **kargs)
 
-    @staticmethod
-    def create(name: str):
-        response = session.post(f"{config['prefix']}/workspaces/{name}", data="null", headers={
-            "Content-Type": "application/json"
-        })
-        if response.ok:
-            return Workspace(name)
+    async def create(self):
+        resp = await self.__request__("post", "", data="null", headers = {"Content-Type": "application/json"})
+        if resp.ok:
+            return await resp.text()
         else:
-            raise RuntimeError(response.text)
+            raise RuntimeError(await resp.text())
 
-    def remove(self):
-        return self.__request__("delete", "")
+    async def remove(self):
+        resp = await self.__request__("delete", "")
+        if resp.ok:
+            content = await resp.text()
+            await self.__session__.close()
+            return content
+        else:
+            raise RuntimeError(f"Failed to remove target workspace: {await resp.text()}")
 
-    def export(self):
-        return self.__request__("get", "/export").json()
+    async def export(self):
+        return await (await self.__request__("get", "/export")).json()
 
-    def get_stacks(self):
-        return self.__request__("get", "/stacks").json()
+    async def get_stacks(self) -> list[int]:
+        return await (await self.__request__("get", "/stacks")).json()
     
-    def get_stack(self, stack_id: int):
-        return self.__request__("get", f"/stacks/{stack_id}").json()
+    async def get_stack(self, stack_id: int) -> Molecule:
+        return await (await self.__request__("get", f"/stacks/{stack_id}")).json()
     
-    def clone_stack(self, stack_id: int) -> int:
-        return self.__request__("post", f"/stacks/{stack_id}/clone_stack").json()
+    async def clone_stack(self, stack_id: int) -> int:
+        return await (await self.__request__("post", f"/stacks/{stack_id}/clone_stack")).json()
     
+    async def is_stack_writable(self, stack_id: int) -> bool:
+        return await (await self.__request__("get", f"/stacks/{stack_id}/writable")).json()
+    
+    async def overlay_fill_layer(self, stack_id: int):
+        return await self.__request__("put", f"/stacks/{stack_id}", json={
+            "Fill": {}
+        })
+        
+    async def write_to_layer(self, stack_id: int, atoms: Atoms, bonds: Bonds):
+        return await self.__request__("patch", f"/stacks/{stack_id}", json=(atoms, bonds))
